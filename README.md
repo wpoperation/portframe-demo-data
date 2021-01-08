b8d497c50e698756f816d2ac5bce91920d61c658
saurab-giri <girisagar051@gmail.com>
2021-01-08 13:21:18 +0545

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# portframe-demo-data
---
+++ config.php
@@ -0,0 +1,511 @@
+<?php
+
+$data = array(
+
+	'portframe' => array(
+		'categories'        => array( 'Portfolio' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/portframe/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+
+	'corporate' => array(
+		'categories'        => array( 'Corporate','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'slider_file'       => 'slider.ss3',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'smart-slider-3',
+					'init'    => 'smart-slider-3/smart-slider-3.php',
+					'name'    => 'Smart Slider',
+				),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+
+
+	'construction' => array(
+		'categories'        => array( 'Corporate','Construction' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-two/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+			),
+		),
+	),
+
+	'restaurant' => array(
+		'categories'        => array( 'Corporate','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-three/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+			),
+		),
+	),
+
+
+	'education' => array(
+		'categories'        => array( 'Corporate','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-four/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'slider_file'       => 'slider.ss3',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'smart-slider-3',
+					'init'    => 'smart-slider-3/smart-slider-3.php',
+					'name'    => 'Smart Slider',
+				),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			
+			),
+		),
+	),
+
+
+	'fitness' => array(
+		'categories'        => array( 'Fitness','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/demo-one/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'Top Menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			
+			),
+		),
+	),
+
+	'spa' => array(
+		'categories'        => array( 'Spa'),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/spa/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Beauty Spa',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> true,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary',
+			'top' 		=> 'top'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+				array(
+					'slug'    => 'woocommerce',
+					'init'    => 'woocommerce/woocommerce.php',
+					'name'    => 'WooCommerce',
+				),
+			
+			),
+		),
+	),
+
+	'portfolio' => array(
+		'categories'        => array( 'Portfolio'),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/portfolio/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary',
+			'top' 		=> 'top'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			
+			),
+		),
+	),
+
+
+	'travel' => array(
+		'categories'        => array( 'Travel'),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/travel/',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Travel',
+		'blog_title'      	=> 'Blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Main Menu',
+			'top' 		=> 'top'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+			),
+		),
+	),
+
+	'charity' => array(
+		'categories'        => array( 'Charity' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/charity',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+
+	'app' => array(
+		'categories'        => array( 'App','One Page' ),
+		'preview_url' 		=> 'https://demo.wpoperation.com/arrival/app',
+		'xml_file'        	=> 'content.xml',
+		'theme_settings'  	=> 'options.dat',
+		'widgets_file'    	=> 'widgets.wie',
+		'home_title'      	=> 'Home',
+		'blog_title'      	=> 'blogs',
+		'posts_to_show'   	=> '5',
+		'is_shop' 			=> false,
+		'menus'         	=> array(
+			'primary' 	=> 'Primary menu'
+		),
+		'required_plugins'  => array(
+			'free' 			=> array(
+				array(
+					'slug'    => 'elementor',
+					'init'    => 'elementor/elementor.php',
+					'name'    => 'Elementor',
+				),
+				array(
+					'slug'    => 'wpop-elementor-addons',
+					'init'    => 'wpop-elementor-addons/wpop-elementor-addons.php',
+					'name'    => 'Operation Elementor Addons',
+				),
+              array(
+                'slug'    => 'ultra-companion',
+                'init'    => 'ultra-companion/ultra-companion.php',
+                'name'    => 'Ultra Companion',
+              ),
+				array(
+					'slug'    => 'contact-form-7',
+					'init'    => 'contact-form-7/wp-contact-form-7.php',
+					'name'    => 'Contact Form 7',
+				),
+			),
+		),
+	),
+	'corporate-pro' => array(
+		'categories'        => array( 'Corporate','Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/demo-one/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'law' => array(
+		'categories'        => array( 'Corporate','Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/demo-two/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'business' => array(
+		'categories'        => array( 'Corporate','Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/demo-three/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'hosting' => array(
+		'categories'        => array( 'one Page','Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/demo-four/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'corporate2' => array(
+		'categories'        => array( 'Corporate','Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/demo-five/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'magazine' => array(
+		'categories'        => array( 'Magazine','Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/demo-six/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'wedding' => array(
+		'categories'        => array( 'Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/wedding/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'gym' => array(
+		'categories'        => array( 'Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/gym/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+	'barbers' => array(
+		'categories'        => array( 'Pro' ),
+		'preview_url'     => 'https://demo.wpoperation.com/arrival-pro/barbers/',
+		'is_premium'  => true,
+		'pro_link' => 'https://wpoperation.com/themes/arrival-pro/',
+		'purchase_link' => 'https://wpoperation.com/cart/?add-to-cart=10539',
+	), 
+);
+
+print_r(json_encode($data));
